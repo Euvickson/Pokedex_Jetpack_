4b0e56a8cb1cbc1e9a08f87fe7fc7bffc716010c
Euvickson <victor.eugennio@gmail.com>
2023-11-29 14:08:47 -0300

    - Refactored the code so the DetailScreen.kt has access to the viewModel
    - Making the pokemon detail API request when entering the DetailScreen.kt

--- app/src/main/java/br/com/euvickson/pokedex_jetpack_/screens/DetailScreen.kt
+++ app/src/main/java/br/com/euvickson/pokedex_jetpack_/screens/DetailScreen.kt
@@ -2,10 +2,16 @@ package br.com.euvickson.pokedex_jetpack_.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import br.com.euvickson.pokedex_jetpack_.screens.viewmodel.PokemonViewModel
 
 @Composable
-fun DetailScreen(int: Int?) {
+fun DetailScreen(int: Int?, viewModel: PokemonViewModel = hiltViewModel()) {
 
     Text(text = int.toString())
+    if (int != null) {
+        viewModel.getPokemonDetail(int)
+    }
 
+    viewModel.pokemonDetail.value.data?.name?.let { Text(text = it) }
 }
